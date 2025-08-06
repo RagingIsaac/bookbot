7ee0cc57f797b099aa40ba676c6d1511a43d08d0
Alexander <alexander.wenzl@digisoolut.com>
2025-08-06 11:48:43 +0200

    Refactor: Sort the list of letters from a book
    - Add new function to stats.py which screates a sorted list
    - Add new function to stats.py which creates an output and only prints alphabetical characters
    - Modify main.py so that new functions are correctly imported

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from stats import get_book_text
 from stats import word_count
 from stats import number_of_characters
+from stats import sort_characters
+from stats import print_report
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
     num_words = word_count(book_text)
     letter_count = number_of_characters(book_text)
-    print(letter_count)
+    sorted_list = sort_characters(letter_count)
+    print_report(book_path, num_words, sorted_list)
 
 main()
--- stats.py
+++ stats.py
@@ -22,3 +22,26 @@ def number_of_characters(text):
         else:
             letters[i] = 1
     return letters
+
+def sort_characters(letters):
+    letter_list = list()
+    for char, count in letters.items():
+        letters_dict = {"char": char, "num": count}
+        letter_list.append(letters_dict)
+    def sort_on(items):
+        return items["num"]
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
+
+def print_report(book_path, num_words, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        char = item["char"]
+        count = item["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+    print("============= END ===============")
