585620419ec7b04cb431471185dcebf680e4f9cc
Alexander <alexander.wenzl@digisoolut.com>
2025-08-06 12:34:31 +0200

    Remove hardcoded book path and add sys arguments to main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_book_text
 from stats import word_count
 from stats import number_of_characters
@@ -5,7 +6,10 @@ from stats import sort_characters
 from stats import print_report
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = word_count(book_text)
     letter_count = number_of_characters(book_text)
